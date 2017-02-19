d050caf0ad3a673807935f1cdaa163e6f5234237
Kaike Zhang <kaikezhang@gmail.com>
2017-02-18 22:00:34 -0500

    api

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -36,8 +36,6 @@ window.axios.defaults.headers.common = {
   'X-Requested-With': 'XMLHttpRequest',
 };
 
-window.axios.defaults.baseURL = 'http://images-sample.dev/api/';
-
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
